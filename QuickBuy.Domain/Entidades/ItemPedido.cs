09ba33a36af8b878c90bc424adc0394e63a6334b
Fernando Costa <fmoc65@gmail.com>
2019-09-25 21:27:24 -0300

    criação das entidades

---
+++ QuickBuy.Domain/Entidades/ItemPedido.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace QuickBuy.Domain.Entidades
+{
+    public class ItemPedido
+    {
+        public int Id { get; set; }
+        public int ProdutoId { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
